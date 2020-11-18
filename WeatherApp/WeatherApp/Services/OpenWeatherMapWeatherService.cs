3c69f592a7718442599431ede0d766f0871c6346
eugene-zhary <eugenezhary05@gmail.com>
2020-11-19 00:17:54 +0200

    first

---
+++ WeatherApp/WeatherApp/Services/OpenWeatherMapWeatherService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherApp.Models;
+
+namespace WeatherApp.Services
+{
+    public class OpenWeatherMapWeatherService : IWeatherService
+    {
+        public async Task<Forecast> GetForecast(double latitude, double longitude)
+        {
+            var language = CultureInfo.CurrentUICulture.TwoLetterISOLanguageName;
+            var apiKey = "b38b0666ec3594a828ac3b8ac707684f";
+            var url = $"https://api.openweathermap.org/data/2.5/forecast?lat={latitude}&lon={longitude}&units=metric&lang={language}&appid={apiKey}";
+
+
+        }
+    }
+}
